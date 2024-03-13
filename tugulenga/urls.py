3098182dcfca0b2af7094d62219b156686681ab9
elainek256 <elaynekkiconco441@gmail.com>
2024-03-13 14:43:53 +0300

    signup page

--- tugulenga/urls.py
+++ tugulenga/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+app_name="tugule"
 urlpatterns=[
-    path('',views.home),
+    path('',views.home,name="home"),
+    path('signup/',views.signup,name="signup")
 ]
--- tugulenga/views.py
+++ tugulenga/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login ,logout
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        
+    
+        
+        username = request.POST.get('username')
+        user = authenticate(username=username, password=request.POST.get('password'))
+        login(request,user)
+            #return redirect('/profile')
     return render(request,'tugulenga/login.html')
+
+def signup(request):
+    form = UserCreationForm()
+    if request.method=='POST':
+        form =UserCreationForm(data=request.POST)
+        if form.is_valid():
+             user =form.save()
+             username = form.cleaned_data.get('username')
+             raw_password = form.cleaned_data.get('password1')
+             user = authenticate(username=username, password=raw_password)
+             login(request,user)
+             return home(request)
+             
+    context={"form":form}
+    return render(request,"tugulenga/signup.html",context)
+    
+    
