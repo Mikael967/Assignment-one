5b63dfb5b895d52107f1b0341bc65e07aa89852d
Michael Rusezera <michaelrusezera@gmail.com>
2024-03-20 19:34:25 +0300

    products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login ,logout
 
 # Create your views here.
+def home(request):
+    
+    return render(request,'products/products.html')
