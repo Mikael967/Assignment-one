1746d2616a885976b5a51105d676cbfc44ebe417
naomiapio11 <naomiapio11@gmail.com>
2024-03-20 23:53:02 +0600

    profile

--- tugulenga/models.py
+++ tugulenga/models.py
@@ -80,6 +80,9 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank = True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.product 
     
 
 
