60251a91d58b37a6e6beac6a126667ab89c7a77d
Pearlkakande <celinpear@gmail.com>
2024-03-22 13:03:16 +0300

    url add product

--- tugulenga/urls.py
+++ tugulenga/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('',views.home,name="home"),
     path('signup/',views.signup,name="signup"),
     path('login/',views.login,name="login"),
+    path('add_product/', views.add_product, name="add_product"),
     
 ]
