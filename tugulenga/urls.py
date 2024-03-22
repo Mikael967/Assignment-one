5fecb2abe14b9f2e379ea6dbfbf2db772af07dd3
Pearlkakande <celinpear@gmail.com>
2024-03-22 15:12:00 +0300

    business account

--- tugulenga/urls.py
+++ tugulenga/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('signup/',views.signup,name="signup"),
     path('login/',views.login,name="login"),
     path('add_product/', views.add_product, name="add_product"),
+    path('add_business_account/', views.add_business_account, name="add_business_account"),
     
 ]
