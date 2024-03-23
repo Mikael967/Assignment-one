28171fd1811c0dc38fb4404c39e1184a4dc69b13
Michael Rusezera <michaelrusezera@gmail.com>
2024-03-23 11:02:10 +0300

    "Added views and urls for product ordering and cart functionality

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name="products"
 urlpatterns=[
     path('',views.home,name="home"),
+    path('order/<str:product_id>',views.create_order_item,name="order"),
+    path('cart/',views.cart,name="cart"),
   
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
-from tugulenga.models import Product
+from django.shortcuts import render,redirect
+from tugulenga.models import Product,Order, OrderItem
 # Create your views here.
+def create_order_item(request,product_id):
+     
+    order=Order.objects.create(customer =  request.user)
+    order.save()
+    product=Product.objects.get(id=product_id)
+    
+
+   
+    
+ 
+    #check if order item already in database
+    try:
+        same_item=OrderItem.objects.get(product=product)
+        same_item.quantity+=1
+        same_item.save()
+
+    except: 
+        order_item=OrderItem.objects.create(
+        product=product,
+        order=order,
+        quantity=1,)
+        order_item.save()
+    
+        
+        
+    
+
+
+    return redirect('products:home')
+
 def home(request):
     products=Product.objects.all()
     context={"products":products}
-    
+
     return render(request,'products/products.html',context)
+def cart(request,):
+    order_items=OrderItem.objects.all()
+    context={"order_items":order_items}
+    return render(request,'products/cart.html',context)
+
