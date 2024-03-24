cf67ee2b4f13fdd2e79a0a16a6ebb8a5dafeb965
Michael Rusezera <michaelrusezera@gmail.com>
2024-03-24 15:36:06 +0300

    forms update

--- tugulenga/forms.py
+++ tugulenga/forms.py
@@ -1,4 +1,7 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import  UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
 from .models import *
 
 
@@ -22,5 +25,8 @@ class OrderItemForm(ModelForm):
     class Meta:
         model = OrderItem
         fields = '__all__'
-               
 
+class  CreateUserForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=['username', 'email','password1', 'password2']
