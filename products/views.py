95c5cb1386a8e4276c6d3ecd95171547e1b38f00
Michael Rusezera <michaelrusezera@gmail.com>
2024-03-24 15:34:02 +0300

    views update

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from tugulenga.models import Product,Order, OrderItem
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def create_order_item(request,product_id):
      
@@ -30,14 +31,26 @@ def create_order_item(request,product_id):
 
 
     return redirect('products:home')
-
+@login_required(login_url='tugule:login')
 def home(request):
     products=Product.objects.all()
     context={"products":products}
 
     return render(request,'products/products.html',context)
+
+@login_required(login_url='tugule:login')
 def cart(request,):
     order_items=OrderItem.objects.all()
     context={"order_items":order_items}
     return render(request,'products/cart.html',context)
+
+@login_required(login_url='tugule:login')
+def deleteOrder(request,product_id):
+    order=OrderItem.objects.get(id=product_id)
+    if request.method=='POST':
+        order.delete()
+        return redirect('products:cart')
+    context={"order":order}
+
+    return render(request,'products/delete.html',context)
 
