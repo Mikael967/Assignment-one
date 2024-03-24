1f3ecf5368cca57bf4dbd3caed5ec28790900f98
Michael Rusezera <michaelrusezera@gmail.com>
2024-03-24 15:33:23 +0300

    urls update

--- products/urls.py
+++ products/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('',views.home,name="home"),
     path('order/<str:product_id>',views.create_order_item,name="order"),
     path('cart/',views.cart,name="cart"),
+    path('delete_order/<str:product_id>',views.deleteOrder,name="delete_order"),
   
 ]
