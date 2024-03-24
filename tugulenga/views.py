95a0265a808a4c222a4693787f9f5925266aa90b
Michael Rusezera <michaelrusezera@gmail.com>
2024-03-24 15:37:18 +0300

    views update

--- tugulenga/views.py
+++ tugulenga/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import  UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login ,logout
+from .forms import CreateUserForm
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
@@ -8,29 +11,40 @@ def home(request):
     return render(request,'tugulenga/index.html')
 
 def signup(request):
-    form = UserCreationForm()
-    if request.method=='POST':
-        form =UserCreationForm(data=request.POST)
-        if form.is_valid():
-             user =form.save()
-             username = form.cleaned_data.get('username')
-             raw_password = form.cleaned_data.get('password1')
-             user = authenticate(username=username, password=raw_password)
-             login(request,user)
-             return home(request)
-             
-    context={"form":form}
-    return render(request,"tugulenga/signup.html",context)
-def login(request):
-    if request.method == 'POST':
-        
-    
-        
-        username = request.POST.get('username')
-        user = authenticate(username=username, password=request.POST.get('password'))
-        login(request,user)
-            #return redirect('/profile')
-    return render(request,'tugulenga/login.html')
+     if request.user.is_authenticated:
+         return redirect('products:home')
+     else:
+        form = CreateUserForm()
+        if request.method=='POST':
+            form =CreateUserForm(data=request.POST)
+            if form.is_valid():
+                    form.save()
+                    user=form.cleaned_data.get('username')
+                    messages.success(request,'Account was created for'+user)
+                    return redirect('tugule:login')
+                
+        context={"form":form}
+        return render(request,"tugulenga/signup.html",context)
+def loginPage(request):
+    if request.user.is_authenticated:
+         return redirect('products:home')
+    else:
+        if request.method == 'POST':
+            
+            username = request.POST.get('username')
+            password=request.POST.get('password')
+            user = authenticate(request,username=username,password=password )
+            if user is not None:
+                login(request,user)
+                return redirect('products:home')
+            else:
+                messages.info(request,'Username or Password is incorrect')
+            
+        return render(request,'tugulenga/login.html')
+def logoutUser(request):
+     logout(request)
+     return redirect('tugule:login')
+
   
   
     
