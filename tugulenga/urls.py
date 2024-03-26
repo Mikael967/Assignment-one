3f3a5ab61b942615d108fa7e51279d593e6b8281
Okello-Arnold <okelloarnold4@gmail.com>
2024-03-27 02:02:52 +0300

    added the url pattern

--- tugulenga/urls.py
+++ tugulenga/urls.py
@@ -7,6 +7,6 @@ urlpatterns=[
     path('login/',views.login,name="login"),
     path('add_product/', views.add_product, name="add_product"),
     path('add_business_account/', views.add_business_account, name="add_business_account"),
-    # path('add_orderItem/', views.add_orderItem, name="add_orderItem")
+    path('add_orderItem/', views.add_orderItem, name="add_orderItem")
     
 ]
