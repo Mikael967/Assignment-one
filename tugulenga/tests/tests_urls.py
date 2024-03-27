32efa9ccfa08d1fea1c1452f246a05e6c794194e
naomiapio11 <naomiapio11@gmail.com>
2024-03-28 00:09:55 +0600

    update test urls

--- tugulenga/tests/tests_urls.py
+++ tugulenga/tests/tests_urls.py
@@ -6,25 +6,25 @@ class TestUrls(SimpleTestCase):
 
     def test_home_url_is_resolved(self): #testing if the url calls the right view function
         url=reverse('tugule:home')
-        self.assertEquals(resolve(url).func, home)
+        self.assertEqual(resolve(url).func, home)
         #print(resolve(url))# to be able to see the expected view function
 
     def test_signup_url_is_resolved(self): #testing if the url calls the right view function
         url=reverse('tugule:signup')
-        self.assertEquals(resolve(url).func, signup)
+        self.assertEqual(resolve(url).func, signup)
 
     def test_login_url_is_resolved(self): #testing if the url calls the right view function
         url=reverse('tugule:login')
-        self.assertEquals(resolve(url).func, loginPage)
+        self.assertEqual(resolve(url).func, loginPage)
 
     def test_add_product_url_is_resolved(self): #testing if the url calls the right view function
         url=reverse('tugule:add_product')
-        self.assertEquals(resolve(url).func, add_product)
+        self.assertEqual(resolve(url).func, add_product)
 
     # def test_add_business_account_url_is_resolved(self): #testing if the url calls the right view function
     #     url=reverse('tugule:add_business_account')
-    #     self.assertEquals(resolve(url).func, add_business_account)
+    #     self.assertEqual(resolve(url).func, add_business_account)
 
     # def test_add_orderItem_url_is_resolved(self): #testing if the url calls the right view function
     #     url=reverse('tugule:add_orderItem')
-    #     self.assertEquals(resolve(url).func, add_orderItem )
+    #     self.assertEqual(resolve(url).func, add_orderItem )
---
+++ tugulenga/tests/tests_views.py
@@ -0,0 +1,15 @@
+# from django.test import TestCase, RequestFactory
+# from django.urls import reverse
+
+# from tugulenga.views import loginPage
+
+
+
+# class TestViews(TestCase):
+#     def setUp(self):
+#         self.view = loginPage
+#         self.factory = RequestFactory()
+
+#     def test_loginPage_with_valid_data(self):
+#         request= self.factory.post(reverse('tugule:login'),data={'username':'Naomi', 'password':'123456789'})
+#         response = self.view(request)
